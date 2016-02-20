925802b29898944019fdecd767af577935835c7a
Isaac Chansky <ichansky@gmail.com>
2016-02-20 17:30:46 -0500

    Update styles to be a bit more in line with current CFB site. (colors/font) Better overlay usability. Better link replacement. Responsive list/calendar switching.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,8 +8,8 @@
   // Constants
 
   var API = {
-    // EVENTS: 'http://localhost:3000/api/v1/events?page=1&page_count=50',
-    EVENTS: 'https://boston-civic-calendar.herokuapp.com/api/v1/events',
+    EVENTS: 'http://localhost:3000/api/v1/events?page=1&page_count=50',
+    // EVENTS: 'https://boston-civic-calendar.herokuapp.com/api/v1/events',
     NEW_EVENT: 'https://boston-civic-calendar.herokuapp.com/events/new',
   };
 
@@ -25,21 +25,23 @@
   var $DOC = $(document);
 
   var DOM = {
-    $tooltips:        $('[data-toggle="tooltip"]'),
-    $newSourceLink:   $('[data-hook="new-event-link"]'),
-    $main:            $('[data-hook=main]'),
-    $eventsPanel:     $('[data-hook=events-panel]'),
-    $errorPanel:      $('[data-hook=error-panel]'),
-    $calendarList:    $('[data-hook="list-container"]'),
-    $calendar:        $('[data-hook="calendar-container"]'),
-    $calendarDetail:  $('[data-hook="calendar-detail"]'),
+    $tooltips: $('[data-toggle="tooltip"]'),
+    $newSourceLink: $('[data-hook="new-event-link"]'),
+    $main: $('[data-hook=main]'),
+    $eventsPanel: $('[data-hook=events-panel]'),
+    $errorPanel: $('[data-hook=error-panel]'),
+    $calendarList: $('[data-hook="list-container"]'),
+    $calendar: $('[data-hook="calendar-container"]'),
+    $calendarDetail: $('[data-hook="calendar-detail"]'),
     $calendarDetailList: $('[data-hook="calendar-detail-events-list"]'),
-    $hideDetail:        $('[data-hook="hide-detail"]')
+    $hideDetail: $('[data-hook="hide-detail"], [data-hook="pane-dim"]'),
+    $listingTab: $('[data-toggle="tab"][href="#listing"]'),
+    $calendarTab: $('[data-toggle="tab"][href="#calendar"]'),
   };
 
 
-  function getTemplateString (idSelector) {
-    return document.getElementById(idSelector).innerHTML ;
+  function getTemplateString(idSelector) {
+    return document.getElementById(idSelector).innerHTML;
   }
 
   var TEMPLATES = {
@@ -48,16 +50,15 @@
 
 
 
-
   // helpers and things
 
-  function dasherize (text) {
+  function dasherize(text) {
     return text.toLowerCase().split(' ').join('-');
   }
 
 
   function transformEventsForFC(events) {
-    return events.map(function(e){
+    return events.map(function(e) {
       return {
         title: e.title,
         start: moment(e.start_date),
@@ -72,33 +73,25 @@
     });
   }
 
-  function linkify (text) {
-    return text.split(' ')
-        .map(function(s){
-          if(s.toLowerCase().indexOf('http') === 0) {
-            return '<a href="'+s+'" target="_blank">'+s+'</a>';
-          } else {
-            return s;
-          }
-        })
-        .join(' ');
+  function linkify(text) {
+    return text.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1">$1</a>');
   }
 
 
 
-  function getEventsOnDate (rawEvents, queryDate) {
+  function getEventsOnDate(rawEvents, queryDate) {
     return rawEvents.filter(function(event) {
       var startDate = moment(event.start_date);
       var endDate = moment(event.end_date);
       // if start or ends on this date
-      if( startDate.dayOfYear() === queryDate.dayOfYear() ||
-          endDate.dayOfYear() === queryDate.dayOfYear() ) {
+      if (startDate.dayOfYear() === queryDate.dayOfYear() ||
+        endDate.dayOfYear() === queryDate.dayOfYear()) {
         return true;
       }
 
       // if event spans over this date
-      if( startDate.dayOfYear() <= queryDate.dayOfYear() &&
-          endDate.dayOfYear() >= queryDate.dayOfYear() ) {
+      if (startDate.dayOfYear() <= queryDate.dayOfYear() &&
+        endDate.dayOfYear() >= queryDate.dayOfYear()) {
         return true;
       }
 
@@ -106,98 +99,109 @@
     });
   }
 
-  function formatEventListItemData (event) {
+  function formatEventListItemData(event) {
     var startDate = moment(event.start_date);
     var endDate = moment(event.end_date);
     var dateString;
-    if( startDate.format('L') === endDate.format('L') ) {
-      dateString = startDate.fromNow() + ' | ' +  startDate.format('L');
+    if (startDate.format('L') === endDate.format('L')) {
+      dateString = startDate.fromNow() + ' | ' + startDate.format('L');
     } else {
-      dateString = startDate.format('L')+' - '+endDate.format('L');
+      dateString = startDate.format('L') + ' - ' + endDate.format('L');
     }
 
     return {
-            title: event.title,
-            organizer: event.organizer,
-            dateString: dateString,
-            location: event.location,
-            description: linkify(event.description)
-          };
+      title: event.title,
+      organizer: event.organizer,
+      dateString: dateString,
+      location: event.location,
+      description: linkify(event.description)
+    };
+  }
+
+
+  function setBestView() {
+    var width = document.documentElement.clientWidth;
+    if (width < 650) {
+      DOM.$listingTab.tab('show');
+    } else {
+      DOM.$calendarTab.tab('show');
+    }
   }
 
 
 
+  // setup bootstrap stuff
 
+  DOM.$tooltips.tooltip();
 
-// setup bootstrap stuff
-
-DOM.$tooltips.tooltip();
-
-DOM.$newSourceLink.attr('href', API.NEW_EVENT);
+  DOM.$newSourceLink.attr('href', API.NEW_EVENT);
 
 
 
-// populate list
-//
+  // populate list
+  //
 
-$.getJSON(API.EVENTS)
-  .error(function(err){
-    console.log(err);
-    DOM.$errorPanel.removeClass('hidden');
-  })
-  .done(function(events){
-    DOM.$eventsPanel.removeClass('hidden');
+  $.getJSON(API.EVENTS)
+    .error(function(err) {
+      console.log(err);
+      DOM.$errorPanel.removeClass('hidden');
+    })
+    .done(function(events) {
+      DOM.$eventsPanel.removeClass('hidden');
 
-    var fullCalEvents = transformEventsForFC(events);
+      var fullCalEvents = transformEventsForFC(events);
 
-    // set up calendar
-    DOM.$calendar.fullCalendar({
-      events: fullCalEvents,
-      theme: false,
-      aspectRatio: 2,
-      header: {
-          left:   'title',
+      // set up calendar
+      DOM.$calendar.fullCalendar({
+        events: fullCalEvents,
+        theme: false,
+        aspectRatio: 2,
+        header: {
+          left: 'title',
           center: 'month,agendaWeek,agendaDay',
-          right:  'today prev,next'
-      },
-      dayClick: function(date) {
-        var eventsOnDay = getEventsOnDate(events, date);
-        $DOC.trigger('calendarDetail:show', { events: eventsOnDay });
-      },
-      eventClick: function(event) {
-        // TODO: this pulls day1 of multi-day events which is wrong if you click on another day... meh. fix soon.
-        var eventsOnDay = getEventsOnDate(events, event._start);
-        $DOC.trigger('calendarDetail:show', { events: eventsOnDay });
-        return false;
-      },
-      eventMouseover: function(event) {
-        $(this).tooltip({
-          title: event.title + ' <br/> ' + event.organizer,
-          html: true
-        });
-        $(this).tooltip('show');
-      },
-      eventMouseout: function() {
-        $(this).tooltip('hide');
-        $(this).tooltip('destroy');
-      }
+          right: 'today prev,next'
+        },
+        dayClick: function(date) {
+          var eventsOnDay = getEventsOnDate(events, date);
+          $DOC.trigger('calendarDetail:show', {
+            events: eventsOnDay
+          });
+        },
+        eventClick: function(event) {
+          // TODO: this pulls day1 of multi-day events which is wrong if you click on another day... meh. fix soon.
+          var eventsOnDay = getEventsOnDate(events, event._start);
+          $DOC.trigger('calendarDetail:show', {
+            events: eventsOnDay
+          });
+          return false;
+        },
+        eventMouseover: function(event) {
+          $(this).tooltip({
+            title: event.title + ' <br/> ' + event.organizer,
+            html: true
+          });
+          $(this).tooltip('show');
+        },
+        eventMouseout: function() {
+          $(this).tooltip('hide');
+          $(this).tooltip('destroy');
+        }
+      });
+
+
+
+      var upcomingEvents = _.chain(events).filter(function(event) {
+        return moment(TODAY).isBefore(event.start_date);
+      }).sortBy('start_date').value();
+
+
+      var tplData = {
+        events: _.map(upcomingEvents, formatEventListItemData)
+      };
+      var tpl = TEMPLATES.calendarList(tplData);
+      DOM.$calendarList.append(tpl);
+
     });
-
-
-
-    var upcomingEvents = _.chain(events).filter(function(event){
-      return moment(TODAY).isBefore(event.start_date);
-    }).sortBy('start_date').value();
-
-
-    var tplData = {
-                    events: _.map(upcomingEvents, formatEventListItemData)
-                  };
-    var tpl = TEMPLATES.calendarList(tplData);
-    DOM.$calendarList.append(tpl);
-
-  });
-
 
 
 
@@ -205,10 +209,10 @@ $.getJSON(API.EVENTS)
 
 
   $DOC.on('calendarDetail:show', function(e, data) {
-     var tplData = {
-                    events: _.map(data.events, formatEventListItemData)
-                  };
-    if( tplData.events.length ) {
+    var tplData = {
+      events: _.map(data.events, formatEventListItemData)
+    };
+    if (tplData.events.length) {
       var tpl = TEMPLATES.calendarList(tplData);
       DOM.$calendarDetailList.html(tpl);
       DOM.$calendarDetail.removeClass('hidden');
@@ -222,10 +226,14 @@ $.getJSON(API.EVENTS)
   });
 
 
-  DOM.$hideDetail.on('click', function(){
+  DOM.$hideDetail.on('click', function() {
     $DOC.trigger('calendarDetail:hide');
   });
 
+
+
+  $(window).on('load resize', _.debounce(function() {
+    setBestView();
+  }, 500));
+
 })(window, document, $, _);
-
-
